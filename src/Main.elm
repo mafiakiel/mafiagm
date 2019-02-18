8fd8885f25646086c646dcc178daf5336c372bec
Erik Hofer <erik-hofer@mail.de>
2019-02-18 16:28:10 +0100

    add elm-bootstrap

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,44 @@
 module App exposing (main)
 
-import Html exposing (div, text)
+import Bootstrap.Button as Button
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Browser
+import Html exposing (Html, div, text)
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+view : Model -> Html Msg
+view model =
+    Grid.container []
+        [ CDN.stylesheet
+        , Button.button [ Button.onClick Decrement, Button.primary ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , Button.button [ Button.onClick Increment, Button.primary ] [ text "+" ]
+        ]
 
 
 main =
-    div [] [ text "hello there!" ]
+    Browser.sandbox
+        { init = 0
+        , view = view
+        , update = update
+        }
