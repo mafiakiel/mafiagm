b8796732352528890141c83a5dc162ace250d6fc
Erik Hofer <erik-hofer@mail.de>
2019-03-08 20:21:37 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import { Elm } from "./Main.elm";
 import registerServiceWorker from "./registerServiceWorker";
 import { Countdown } from "./countdown";
 
-// Polyfill because Firefox doesn't support custom elements yet
-// import '@webcomponents/custom-elements'
-// import '@webcomponents/webcomponentsjs'
-
 customElements.define("x-countdown", Countdown);
 
 const seed = Math.floor(Math.random() * 10000);
